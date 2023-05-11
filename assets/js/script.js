69c972e6aad1f668452e4d713f1242d322fd34ed
nmjs2023 <nm.js.2023@gmail.com>
2023-05-11 09:09:05 -0400

    primer commit, con estructura básica de html

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var presupuesto = {
+    presupuestoInicial: 0,
+    totalGastos: 0,
+    detalleGastos: [{descripcion:'a',montoGasto:0}, {descripcion:'b',montoGasto:1}]
+};
+
+console.log(presupuesto);
+console.log(presupuesto.detalleGastos[0].descripcion);
+console.log(presupuesto.detalleGastos[1].descripcion);
