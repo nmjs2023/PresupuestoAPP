7a8c228c0380b2977703d5991c17d0b1fe335906
nmjs2023 <nm.js.2023@gmail.com>
2023-05-18 09:18:53 -0400

    se corrije calculo total de gastos, se renderiza tabla resumen de presupuesto, se renderiza tabla de detalle de gastos, se agrega icono para eliminación de gastos

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,7 @@ const presupuesto = {
         let suma = 0;
         presupuesto.detalleGastos.forEach(gasto => {
             suma += gasto.montoGasto;
+            //suma += gasto[1];
         });
         return suma;
     }
@@ -38,20 +39,70 @@ btnAceptarPresupuesto.addEventListener('click', function () {
     }
     /* let montoPresupuesto = document.getElementById('montoPresupuesto'); */
     const montoPresupuesto = document.querySelector('#montoPresupuesto');
-    presupuesto.presupuestoInicial = parseInt(montoPresupuesto.value,10);  //en base 10 --> notación decimal
+    /* en base 10 --> notación decimal */
+    presupuesto.presupuestoInicial = parseInt(montoPresupuesto.value,10);  
     renderResumenPresupuesto();
     
     /* console.log('--- Fin Evento Click Presupuesto'); */
 });
 
+function eliminaNodos(pTag) {
+    /*  Elimina los hijos de un determinado Tag.    */
+    
+    console.log("dentro de eliminaNodos -->" + pTag)
+    var elemento = document.getElementById(pTag);
+    while (elemento.firstChild) {
+        elemento.removeChild(elemento.firstChild);
+    }
+
+};
 
 function renderResumenPresupuesto ()  {
     thMontoPresupuesto.innerText = presupuesto.presupuestoInicial;
     thTotalGastos.innerText = presupuesto.totalGastos();
     thSaldoPresupuesto.innerText = presupuesto.presupuestoInicial - presupuesto.totalGastos();
+};
+
+function renderDetalleGastos() {
+    //sólo se renderizan las filas de detalle de gastos (no los encabezados)
+
+    //primero se eliminan filas de detalle de la tabla de gastos (si es que existen)
+    eliminaNodos('tBodyDetalleGasto');
+    const tBodyDetalleGasto = document.querySelector('#tBodyDetalleGasto');
+
+    let cuenta =0;
+    presupuesto.detalleGastos.forEach(function (gastoAux) {
+        const fila = document.createElement('tr');
+        const colDescGasto = document.createElement('td');
+        const colValorGasto = document.createElement('td');
+        const colEliminaGasto = document.createElement('td');
+        const btnEliminarGasto = document.createElement('button');
+        
+        
+        btnEliminarGasto.setAttribute('id', `btnEliminar${cuenta}`);
+        btnEliminarGasto.setAttribute('class', 'btn rounded-fill');
+
+        
+
+        const iconoEliminar = document.createElement('i');
+        iconoEliminar.setAttribute('class', 'bi bi-trash')
+
+
+
+        colDescGasto.innerHTML = gastoAux.descripcionGasto;
+        colValorGasto.innerHTML = gastoAux.montoGasto;
+
+        btnEliminarGasto.appendChild(iconoEliminar);
+        colEliminaGasto.appendChild(btnEliminarGasto);
+
+        fila.append(colDescGasto, colValorGasto, colEliminaGasto);
+        tBodyDetalleGasto.append(fila);
+    })    
+    
 }
 
 btnAceptarGasto = document.getElementById('btnAceptarGasto');
+
 btnAceptarGasto.addEventListener('click', function () {
     const descripcionGasto = document.querySelector('#descripcionGasto');
     console.log(`descrip del gasto capturado: ${descripcionGasto.value}`);
@@ -59,8 +110,12 @@ btnAceptarGasto.addEventListener('click', function () {
     const montoGasto = document.querySelector('#montoGasto');
     console.log(`monto del gasto capturado: ${montoGasto.value}`);
 
+   
     agregarGastoArreglo(descripcionGasto.value, parseInt(montoGasto.value));
+
+
     renderResumenPresupuesto();
+    renderDetalleGastos();
 });
 
 
